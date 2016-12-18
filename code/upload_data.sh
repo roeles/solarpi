9be59a40470d9f2aaf9dab4e7456b3e80ea8cddd
root <root@solarpi.cumulus-humilis.nl>
2016-12-18 11:29:50 +0000

    Enable/disable wifi around data uploading.

--- code/upload_data.sh
+++ code/upload_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ifup wlan0
+sleep 10
+
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 cd $MY_PATH
@@ -7,3 +10,4 @@ cd ..
 
 rsync ./data/solarpi.csv rsync://192.168.178.36/data/solarpi
 
+ifdown wlan0
