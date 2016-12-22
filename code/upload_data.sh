ee03eb4797bba7c7e56a49fc3eca0e5c930c8c51
Roel Baardman <roel.baardman@gmail.com>
2016-12-22 15:16:58 +0000

    Use power saving for wifi.

--- code/upload_data.sh
+++ code/upload_data.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sleep 10
+
+/sbin/ifup wlan0
+
 
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 cd $MY_PATH
 cd ..
 
+git pull
+make
+
 rsync ./data/solarpi.csv rsync://192.168.178.36/data/solarpi
 
+sleep 60
+/sbin/ifdown wlan0 
